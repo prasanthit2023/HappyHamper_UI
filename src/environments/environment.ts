6b61fbc4171de89980e14fa6511131df1fd1732c
Prasanth Ramkumar <WS1159@wsad.cn>
2026-06-24 23:47:45 +0530

    Update development API URL to target live Azure backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7226/api/v1',
+  apiUrl: 'https://happyhamper-ayhnh0geczaretd8.centralindia-01.azurewebsites.net/api/v1',
   googleClientId: '36993605748-g7h295kjk8as8slf7a12l6lo1ekpogrs.apps.googleusercontent.com',
   razorpayKeyId: 'YOUR_RAZORPAY_KEY_ID',
 };
