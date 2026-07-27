dce67fda37aed6857861d3affc14c5316d5470ef
firststepfs26-git <firststepfs26@gmail.com>
2026-07-27 19:46:12 +0530

    Update Razorpay Key ID in environments to live mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://happyhamper-ayhnh0geczaretd8.centralindia-01.azurewebsites.net/api/v1',
   googleClientId: '36993605748-g7h295kjk8as8slf7a12l6lo1ekpogrs.apps.googleusercontent.com',
-  razorpayKeyId: 'YOUR_RAZORPAY_KEY_ID',
+  razorpayKeyId: 'rzp_live_TIYAdbeY5JYxYQ',
 };
